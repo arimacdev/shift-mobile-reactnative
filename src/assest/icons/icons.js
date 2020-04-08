d4482c5b08cb08326006db9d1cf8d96a3fc40ebc
Danushka Sandaruwan <dhanushka.sandaruwan@arimaclanka.com>
2020-04-08 19:24:11 +0530

    Create login screen

---
+++ src/assest/icons/icons.js
@@ -0,0 +1,5 @@
+const icons = {
+    user: require('./user.png'),
+    password: require('./password.png')
+};
+export default icons;
--- src/config/colors.js
+++ src/config/colors.js
@@ -5,6 +5,10 @@ const colors = {
     drawerColor : '#0C0C5A',
     drawerActiveItems : '#FFFFFF',
     drawerInactiveItems : '#4D4F85',
-    pageBackGroundColor : '#E5E5E5'
+    pageBackGroundColor : '#E5E5E5',
+    darkBlue:'#1a0070',
+    lightBlue: '#38afff',
+    lightgray: '#D3D3D3',
+    lighterGray: '#e5e5e5',
 };
 export default colors;
