1085475ee05a74645eae421f4fe42c2ddf9d2ad3
Danushka Sandaruwan <dhanushka.sandaruwan@arimaclanka.com>
2020-04-09 18:26:05 +0530

    Create task page

--- src/assest/icons/icons.js
+++ src/assest/icons/icons.js
@@ -3,6 +3,8 @@ const icons = {
     password: require('./password.png'),
     folder: require('./folder.png'),
     arrow: require('./arrow.png'),
-    arrowDark: require('./arrow_dark.png')
+    arrowDark: require('./arrow_dark.png'),
+    whiteCircule: require('./white_circule.png'),
+    rightCircule: require('./right_circule.png')
 };
 export default icons;
--- src/screens/App/Tasks/BordTabScreen.js
+++ src/screens/App/Tasks/BordTabScreen.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import {View, FlatList, Text, Dimensions, Image, TouchableOpacity} from 'react-native';
+import {
+  View,
+  FlatList,
+  Text,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import {connect} from 'react-redux';
 import * as actions from '../../../redux/actions';
 import colors from '../../../config/colors';
@@ -7,21 +14,28 @@ import icons from '../../../assest/icons/icons';
 import EStyleSheet from 'react-native-extended-stylesheet';
 const entireScreenWidth = Dimensions.get('window').width;
 EStyleSheet.build({$rem: entireScreenWidth / 380});
-import { Dropdown } from 'react-native-material-dropdown';
+import {Dropdown} from 'react-native-material-dropdown';
 
-let dropData = [{
+let dropData = [
+  {
     value: 'Status',
-  }, {
+  },
+  {
     value: 'Completed',
-  }, {
+  },
+  {
     value: 'Not started',
-  }, {
+  },
+  {
     value: 'QA',
-  }, {
+  },
+  {
     value: 'Unassigned',
-  }, {
+  },
+  {
     value: 'Assigned',
-  }];
+  },
+];
 
 class BordTabScreen extends Component {
   constructor(props) {
@@ -33,32 +47,36 @@ class BordTabScreen extends Component {
           taskName: 'Home page login',
           taskStatus: 'Ongoing',
           taskStatusColor: '#ffc213',
-          taskDate:'Yesterday',
-          avatr:icons.folder
+          taskCompletion: false,
+          taskDate: 'Yesterday',
+          avatr: icons.whiteCircule,
         },
         {
           taskId: '0002',
           taskName: 'Home page login',
           taskStatus: 'Ongoing',
-          taskStatusColor: '#ffc213',
-          taskDate:'Yesterday',
-          avatr:icons.folder
+          taskStatusColor: 'red',
+          taskCompletion: false,
+          taskDate: 'Today',
+          avatr: icons.folder,
         },
         {
           taskId: '0003',
           taskName: 'Home page login',
           taskStatus: 'Ongoing',
-          taskStatusColor: '#ffc213',
-          taskDate:'Yesterday',
-          avatr:icons.folder
+          taskStatusColor: 'gray',
+          taskCompletion: false,
+          taskDate: '2020/02/03',
+          avatr: icons.folder,
         },
         {
           taskId: '0004',
           taskName: 'Home page login',
           taskStatus: 'Ongoing',
-          taskStatusColor: '#ffc213',
-          taskDate:'Yesterday',
-          avatr:icons.folder
+          taskStatusColor: 'gray',
+          taskCompletion: true,
+          taskDate: '2020/01/12',
+          avatr: icons.folder,
         },
       ],
     };
@@ -70,57 +88,64 @@ class BordTabScreen extends Component {
 
   renderProjectList(item) {
     return (
-    <TouchableOpacity onPress={()=>this.props.navigation.navigate('TasksScreen',{taskDetails:item})}>
-      <View style={styles.projectView}>
-        <Image style={styles.userIcon} source={item.avatr} />
-        <View style={{flex:1}}>
+      <TouchableOpacity
+        onPress={() =>
+          this.props.navigation.navigate('TasksScreen', {taskDetails: item})
+        }>
+        <View style={styles.projectView}>
+          <Image
+            style={styles.completionIcon}
+            source={
+              item.taskCompletion == true ? icons.rightCircule : icons.whiteCircule
+            }
+          />
+          <View style={{flex: 1}}>
             <Text style={styles.text}>{item.taskName}</Text>
+          </View>
+          <View style={styles.statusView}>
+            <Text style={[styles.textDate, {color:item.taskStatusColor}]}>{item.taskDate}</Text>
+            <Image style={styles.avatarIcon} source={item.avatr} />
+          </View>
         </View>
-        <View style={styles.statusView}>
-          <Text style={styles.text}>{item.projName}</Text>
-          <Image style={styles.userIcon} source={item.avatr} />
-        </View>
-      </View>
       </TouchableOpacity>
     );
   }
 
-renderBase(){
-    return(
-        <View style={{justifyContent:'center',flex:1}}>
-           <Image style={styles.dropIcon} source={icons.arrowDark} />
-        </View>
-    )
-}
+  renderBase() {
+    return (
+      <View style={{justifyContent: 'center', flex: 1}}>
+        <Image style={styles.dropIcon} source={icons.arrowDark} />
+      </View>
+    );
+  }
 
   render() {
     return (
       <View style={styles.backgroundImage}>
         <View style={styles.projectFilerView}>
-        <Dropdown
-        // style={{}}
-        label=''
-        labelFontSize={0}
-        data={dropData}
-        textColor={colors.lightgray}
-        error={''}
-        animationDuration={0.5}
-        containerStyle={{width:'100%'}}
-        overlayStyle={{width:'100%'}}
-        pickerStyle={{width:'89%',marginTop:70, marginLeft:15}}
-        dropdownPosition={0}
-        value={'Status'}
-        itemColor={'black'}
-        selectedItemColor={'black'}
-        dropdownOffset={{top:10}}
-        baseColor={colors.projectBgColor}
-        // renderBase={this.renderBase}
-        renderAccessory={this.renderBase}
-        itemTextStyle={{marginLeft:15}}
-        itemPadding={10}
-
-      />
-            {/* <TouchableOpacity>
+          <Dropdown
+            // style={{}}
+            label=""
+            labelFontSize={0}
+            data={dropData}
+            textColor={colors.lightgray}
+            error={''}
+            animationDuration={0.5}
+            containerStyle={{width: '100%'}}
+            overlayStyle={{width: '100%'}}
+            pickerStyle={{width: '89%', marginTop: 70, marginLeft: 15}}
+            dropdownPosition={0}
+            value={'Status'}
+            itemColor={'black'}
+            selectedItemColor={'black'}
+            dropdownOffset={{top: 10}}
+            baseColor={colors.projectBgColor}
+            // renderBase={this.renderBase}
+            renderAccessory={this.renderBase}
+            itemTextStyle={{marginLeft: 15}}
+            itemPadding={10}
+          />
+          {/* <TouchableOpacity>
                 <View>
                     <Text style={styles.textFilter}>Ongoing</Text>
                 </View>
@@ -141,18 +166,18 @@ const styles = EStyleSheet.create({
     flex: 1,
     // backgroundColor: colors.pageBackGroundColor,
   },
-  projectFilerView:{
+  projectFilerView: {
     backgroundColor: colors.projectBgColor,
     borderRadius: 5,
     // width: '330rem',
     marginTop: '17rem',
-    marginBottom:'12rem',
+    marginBottom: '12rem',
     flexDirection: 'row',
     alignItems: 'center',
     // justifyContent: 'center',
     paddingHorizontal: '12rem',
     height: '45rem',
-    marginHorizontal:'20rem'
+    marginHorizontal: '20rem',
   },
   textFilter: {
     fontSize: '14rem',
@@ -166,12 +191,12 @@ const styles = EStyleSheet.create({
   projectView: {
     backgroundColor: colors.projectBgColor,
     borderRadius: 5,
-    height:'60rem',
+    height: '60rem',
     marginTop: '7rem',
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: '12rem',
-    marginHorizontal:'20rem'
+    marginHorizontal: '20rem',
   },
   text: {
     fontSize: '12rem',
@@ -181,24 +206,39 @@ const styles = EStyleSheet.create({
     lineHeight: '17rem',
     fontFamily: 'HelveticaNeuel',
     textAlign: 'left',
-    marginLeft:'10rem'
+    marginLeft: '10rem',
   },
-  userIcon: {
+  textDate:{
+    fontSize: '9rem',
+    color: colors.projectText,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    lineHeight: '17rem',
+    fontFamily: 'HelveticaNeuel',
+    textAlign: 'left',
+    marginLeft: '10rem',
+  },
+  avatarIcon: {
     width: '20rem',
     height: '20rem',
+    marginLeft: 10
   },
-  statusView:{
+  statusView: {
     // backgroundColor: colors.gray,
     // width:'5rem',
     // height:'60rem',
-    alignItems:'center',
-    flexDirection:'row',
+    alignItems: 'center',
+    flexDirection: 'row',
     // borderTopRightRadius: 5,
     // borderBottomRightRadius: 5,
   },
-  dropIcon:{
+  dropIcon: {
     width: '13rem',
     height: '13rem',
+  },
+  completionIcon:{
+    width: '40rem',
+    height: '40rem',
   }
 });
 
