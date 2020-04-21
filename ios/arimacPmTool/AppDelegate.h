bc4fc1c7da081d77126e9bc69f300d02d33de42f
asiri_piyajanaka <asiripiyajanaka@gmail.com>
2020-04-21 20:07:39 +0530

    iOS build.

--- ios/arimacPmTool/AppDelegate.h
+++ ios/arimacPmTool/AppDelegate.h
@@ -1,8 +1,11 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import "RNAppAuthAuthorizationFlowManager.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, RNAppAuthAuthorizationFlowManager>
+// @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
+@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 @property (nonatomic, strong) UIWindow *window;
 
 @end
