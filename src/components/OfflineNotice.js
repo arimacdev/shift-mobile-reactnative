4e2177c8bedd14542d238986ea7525f916c45063
Thilina Warnakulasuriya <thilina9292.contact@gmail.com>
2020-05-25 13:50:58 +0530

    add app icon,offilen page

---
+++ src/components/OfflineNotice.js
@@ -0,0 +1,87 @@
+
+import React, { Component } from 'react';
+import { View, Text, Dimensions,Image } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import NetInfo from "@react-native-community/netinfo";
+import colors from '../config/colors';
+
+const { width,height } = Dimensions.get('window');
+const entireScreenWidth = Dimensions.get('window').width;
+EStyleSheet.build({ $rem: entireScreenWidth / 380 });
+
+function MiniOfflineSign() {
+    return (
+        <View style={styles.offlineContainer} pointerEvents={'box-only'}>
+            <Text style={styles.offlineText}>No Internet Connection</Text>
+        </View>
+    );
+}
+
+class OfflineNotice extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isConnected: true
+        };
+        //initial network state
+        
+    }
+
+    componentDidMount() {
+        const unsubscribe = NetInfo.addEventListener(state => {
+            //console.log("Connection type", state.type);
+            //console.log("Is connected?", state.isConnected);
+            this.handleConnectivityChange(state.isConnected)
+        });
+    }
+
+    componentWillUnmount() {
+        unsubscribe();
+    }
+
+    handleConnectivityChange = isConnected => {
+        if (isConnected) {
+            this.setState({ isConnected });
+        } else {
+            this.setState({ isConnected });
+        }
+    };
+
+    render() {
+        if (!this.state.isConnected) {
+            return <MiniOfflineSign />;
+        }
+        return null;
+    }
+}
+
+const styles = EStyleSheet.create({
+    offlineContainer: {
+        backgroundColor: colors.white,
+        height: height,
+        elevation: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        width,
+        position: 'absolute',
+        top: 0,
+        zIndex: 1
+    },
+    offlineText: {
+         color: colors.primary, 
+         fontFamily: 'HelveticaNeueMedium',
+         marginTop : 5
+         //backgroundColor : 'red',
+         //padding : 10,
+        // borderRadius :  5
+    },
+    image: {
+        width: '90rem',
+        height: '90rem',
+        resizeMode: 'contain',
+    }
+});
+
+export { OfflineNotice };
