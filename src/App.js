0129d533c7dce77ef2eb5e713fd9471b1460b527
Danushka Sandaruwan <dhanushka.sandaruwan@arimaclanka.com>
2020-06-08 17:06:04 +0530

    Configure one signal notification

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
-
-
 import React, {Component} from 'react';
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from '../src/redux/reducers';
 import NavigationService from './services/NavigationService';
-import { MainSwitchNavigator } from './navigators/MainSwitchNavigator';
-import { OfflineNotice } from './components/OfflineNotice';
+import {MainSwitchNavigator} from './navigators/MainSwitchNavigator';
+import {OfflineNotice} from './components/OfflineNotice';
 import OneSignal from 'react-native-onesignal';
 
 console.disableYellowBox = true;
 
-
 export default class App extends Component {
-
   constructor(properties) {
     super(properties);
-    this.state = {
-    };
+    this.state = {};
     this.store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-  
+    //Remove this method to stop OneSignal Debugging
+    OneSignal.setLogLevel(6, 0);
+
+    // Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+    OneSignal.init('YOUR_ONESIGNAL_APP_ID', {
+      kOSSettingsKeyAutoPrompt: false,
+      kOSSettingsKeyInAppLaunchURL: false,
+      kOSSettingsKeyInFocusDisplayOption: 2,
+    });
+    OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
+
+    // The promptForPushNotifications function code will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step below)
+    // OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
   }
- 
-  componentWillMount(){
+
+  componentWillMount() {}
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
+  myiOSPromptCallback(permission) {
+    // do something with permission value
   }
 
   render() {
@@ -32,9 +67,9 @@ export default class App extends Component {
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
         {
           <MainSwitchNavigator
-              ref={navigatorRef => {
-                NavigationService.setTopLevelNavigator(navigatorRef);
-              }}
+            ref={navigatorRef => {
+              NavigationService.setTopLevelNavigator(navigatorRef);
+            }}
           />
         }
         <OfflineNotice />
@@ -42,4 +77,3 @@ export default class App extends Component {
     );
   }
 }
-
