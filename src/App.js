d306e9d5be6496e89485c40d69a89a5fca423db9
asiri_piyajanaka <asiripiyajanaka@gmail.com>
2020-06-10 09:16:34 +0530

    one Signal iOS done.

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default class App extends Component {
     OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
 
     // The promptForPushNotifications function code will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step below)
-    // OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+    OneSignal.promptForPushNotificationsWithUserResponse(this.myiOSPromptCallback);
 
     OneSignal.addEventListener('received', this.onReceived);
     OneSignal.addEventListener('opened', this.onOpened);
