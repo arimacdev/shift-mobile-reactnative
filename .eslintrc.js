c8d53f54250e8ee2c19876594139cac9c80cd7d8
Danushka Sandaruwan <dhanushka.sandaruwan@arimaclanka.com>
2020-07-28 13:58:38 +0530

    Added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,28 @@
 module.exports = {
   root: true,
+  plugins: ['react', 'react-native'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    'react-native/react-native': true,
+  },
   extends: '@react-native-community',
+  settings: {
+    'react-native/style-sheet-object-names': [
+      'EStyleSheet',
+      'OtherStyleSheet',
+      'PStyleSheet',
+    ],
+  },
+  rules: {
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-color-literals': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+  },
 };
