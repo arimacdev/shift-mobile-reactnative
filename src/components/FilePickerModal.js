d0f0f54d9a53fc536bece767deb206b705ba7427
Danushka Sandaruwan <dhanushka.sandaruwan@arimaclanka.com>
2020-09-15 11:26:15 +0530

    Remove unused imports

--- src/components/FilePickerModal.js
+++ src/components/FilePickerModal.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Dimensions, View, Text, TouchableOpacity, Image} from 'react-native';
+import {Dimensions, View, Text, TouchableOpacity} from 'react-native';
 import {connect} from 'react-redux';
 import * as actions from '../redux/actions';
 import colors from '../config/colors';
@@ -7,7 +7,6 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const entireScreenWidth = Dimensions.get('window').width;
 EStyleSheet.build({$rem: entireScreenWidth / 380});
 import Modal from 'react-native-modal';
-import icons from '../asserts/icons/icons';
 
 class FilePickerModal extends Component {
   constructor(props) {
